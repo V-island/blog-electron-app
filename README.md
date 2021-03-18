820ce890a4251e7a8d906a2965c9f1cd079aec33
lv <island@1lift.cn>
2021-03-18 18:07:24 +0800

    创建VUE项目

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# electron-app
-采用 Electron 封装尝试开发客户端应用程序
+# blog-electron-app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+//窗口配置程序运行窗口的大小
+const windowConfig = {
+  width:800,
+  height:600
+};
 
-  win.loadFile('index.html')
+function createWindow () {s
+  const win = new BrowserWindow(windowConfig)
+
+  // win.loadFile('index.html')
+  win.loadURL(`file://${__dirname}/index.html`);//在窗口内要展示的内容index.html 就是打包生成的index.html
+  win.webContents.openDevTools();  //开启调试工具
+  win.on('close',() => {
+    //回收BrowserWindow对象
+    win = null;
+  });
+  win.on('resize',() => {
+    win.reload();
+  })
 }
 
 app.whenReady().then(createWindow)
